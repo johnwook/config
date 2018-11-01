d74dbb8f90b64fad05f7f21ae494c86c18dee21d
Johnwook Choi <johnwook.choi@gmail.com>
2018-11-01 12:17:56 +0900

    update & install fzf fish
    - update fisher packages
    - add fzf fish binding, see https://github.com/jethrokuan/fzf

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,3 @@
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
-
-set -gx LANG en_US
