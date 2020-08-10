72de63aef79edf22c25ea18052c362e4a4df70dc
Johnwook Choi <johnwook.choi@gmail.com>
2020-08-10 15:30:19 +0900

    install gcloud and update fish variables

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,5 @@
 starship init fish | source
 source (brew --prefix asdf)/asdf.fish
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/johnwook/google-cloud-sdk/path.fish.inc' ]; . '/Users/johnwook/google-cloud-sdk/path.fish.inc'; end
