6f31d1aed0ce28111c14264f6e73ec583fc2b3ff
Johnwook Choi <johnwook.choi@gmail.com>
2023-06-23 15:28:44 +0900

    updato lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -49,6 +49,7 @@ require("lazy").setup({
   { "ms-jpq/coq.artifacts", branch = "artifacts" },
   { "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   "jose-elias-alvarez/typescript.nvim",
+  { "windwp/nvim-autopairs", event = "InsertEnter" },
 })
 
 -- Plugin specific configurations
@@ -73,7 +74,6 @@ require("mini.basics").setup()
 require("mini.bracketed").setup()
 require("mini.cursorword").setup()
 require("mini.indentscope").setup()
-require("mini.pairs").setup()
 require("mini.tabline").setup()
 require("mini.trailspace").setup()
 require("which-key").setup()
@@ -151,6 +151,45 @@ null_ls.setup({
     end
   end,
 })
+
+-- Autopairs with coq (https://github.com/windwp/nvim-autopairs)
+local remap = vim.api.nvim_set_keymap
+local npairs = require("nvim-autopairs")
+
+npairs.setup({ map_bs = false, map_cr = false })
+
+vim.g.coq_settings = { keymap = { recommended = false } }
+
+-- these mappings are coq recommended mappings unrelated to nvim-autopairs
+remap("i", "<esc>", [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+remap("i", "<c-c>", [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+remap("i", "<tab>", [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+remap("i", "<s-tab>", [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+
+-- skip it, if you use another global object
+_G.MUtils= {}
+
+MUtils.CR = function()
+  if vim.fn.pumvisible() ~= 0 then
+    if vim.fn.complete_info({ "selected" }).selected ~= -1 then
+      return npairs.esc("<c-y>")
+    else
+      return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+    end
+  else
+    return npairs.autopairs_cr()
+  end
+end
+remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+
+MUtils.BS = function()
+  if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ "mode" }).mode == "eval" then
+    return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+  else
+    return npairs.autopairs_bs()
+  end
+end
+remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
 
 -- Keymaps
 -- lsp config
