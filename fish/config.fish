456d9b698a22fdfc8ed6df9ae3e5236e221ad1f4
Johnwook Choi <johnwook.choi@gmail.com>
2023-08-18 13:52:57 +0900

    use formatter instead of null-ls

--- fish/config.fish
+++ fish/config.fish
@@ -25,12 +25,12 @@ set ZELLIJ_AUTO_EXIT true
 set SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
 # Homebrew
-set -gx HOMEBREW_PREFIX "/opt/homebrew";
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+# set -gx HOMEBREW_PREFIX "/opt/homebrew";
+# set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+# set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+# set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+# set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+# set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 
 # pnpm
 set -gx PNPM_HOME "/Users/johnwook/Library/pnpm"
