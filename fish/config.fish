4370907d52f417b36bcd5935d0f184e86e6c9ed5
Johnwook Choi <johnwook.choi@gmail.com>
2024-04-08 11:42:20 +0900

    update gitignore and config

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,11 @@
 if status is-interactive
-    eval (zellij setup --generate-auto-start fish | string collect)
+  # 
 end
 
 source /opt/local/share/asdf/asdf.fish
-source /opt/local/share/fzf/shell/key-bindings.fish
 
+eval "$(/opt/homebrew/bin/brew shellenv)"
+fzf --fish | source
 starship init fish | source
 zoxide init fish | source
 
@@ -24,17 +25,13 @@ set ZELLIJ_AUTO_EXIT true
 # SSH
 set SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 
-# Homebrew
-# set -gx HOMEBREW_PREFIX "/opt/homebrew";
-# set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-# set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-# set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-# set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-# set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
-
 # pnpm
 set -gx PNPM_HOME "/Users/johnwook/Library/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+if status is-interactive
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
