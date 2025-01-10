a8ce3cbd543b94233c5a696949d463c0e62dc56a
최종욱 <johnwook@choejong-ug-ui-Macmini.local>
2025-01-10 17:47:07 +0900

    support nvim-lsp

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -97,6 +97,7 @@ require("lazy").setup({
 				"hrsh7th/cmp-path",
 				"hrsh7th/cmp-calc",
 				"hrsh7th/cmp-cmdline",
+				"hrsh7th/cmp-nvim-lsp",
 			},
 			config = function()
 				local cmp = require("cmp")
@@ -146,6 +147,7 @@ require("lazy").setup({
 						end, { "i", "s" }),
 					}),
 					sources = cmp.config.sources({
+						{ name = "nvim_lsp" },
 						{ name = "luasnip" },
 					}, {
 						{ name = "buffer" },
@@ -305,9 +307,13 @@ require("lazy").setup({
 	checker = { enabled = true },
 })
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
-		require("lspconfig")[server_name].setup({})
+		require("lspconfig")[server_name].setup({
+			capabilities = capabilities,
+		})
 	end,
 })
 
