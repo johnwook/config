ed7bed8459afde951de9cfbdf5c68b117e758cb8
최종욱 <johnwook@choejong-ug-ui-Macmini.local>
2025-01-10 17:20:27 +0900

    coq => nvim-cmp

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -59,7 +59,7 @@ require("lazy").setup({
 				},
 			},
 		},
-		{ "nvim-tree/nvim-web-devicons", opts = {} },
+		{ "nvim-tree/nvim-web-devicons", lazy = true },
 		{ "ibhagwan/fzf-lua", opts = {} },
 		{ "williamboman/mason.nvim", opts = {} },
 		{
@@ -85,6 +85,104 @@ require("lazy").setup({
 		},
 		{
 			"neovim/nvim-lspconfig",
+		},
+		{
+			"hrsh7th/nvim-cmp",
+			event = "InsertEnter",
+			dependencies = {
+				"L3MON4D3/LuaSnip",
+				"saadparwaiz1/cmp_luasnip",
+				"hrsh7th/cmp-buffer",
+				"hrsh7th/cmp-emoji",
+				"hrsh7th/cmp-path",
+				"hrsh7th/cmp-calc",
+				"hrsh7th/cmp-cmdline",
+			},
+			config = function()
+				local cmp = require("cmp")
+				local luasnip = require("luasnip")
+
+				cmp.setup({
+					snippet = {
+						expand = function(args)
+							luasnip.lsp_expand(args.body)
+						end,
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								if luasnip.expandable() then
+									luasnip.expand()
+								else
+									cmp.confirm({
+										select = true,
+									})
+								end
+							else
+								fallback()
+							end
+						end),
+						["<Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.locally_jumpable(1) then
+								luasnip.jump(1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+						["<S-Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.locally_jumpable(-1) then
+								luasnip.jump(-1)
+							else
+								fallback()
+							end
+						end, { "i", "s" }),
+					}),
+					sources = cmp.config.sources({
+						{ name = "luasnip" },
+					}, {
+						{ name = "buffer" },
+						{ name = "emoji" },
+						{ name = "path" },
+						{ name = "calc" },
+					}),
+				})
+
+				cmp.setup.cmdline("/", {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = {
+						{ name = "buffer" },
+					},
+				})
+
+				cmp.setup.cmdline(":", {
+					mapping = cmp.mapping.preset.cmdline(),
+					sources = cmp.config.sources({
+						{ name = "path" },
+					}, {
+						{
+							name = "cmdline",
+							option = {
+								ignore_cmds = { "Man", "!" },
+							},
+						},
+					}),
+				})
+			end,
+		},
+		{
+			"L3MON4D3/LuaSnip",
+			-- follow latest release.
+			version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+			-- install jsregexp (optional!).
+			build = "make install_jsregexp",
 		},
 		{
 			"folke/lazydev.nvim",
@@ -201,6 +299,7 @@ require("lazy").setup({
 			opts = {},
 		},
 		{ "lewis6991/gitsigns.nvim", opts = {} },
+		{ "stevearc/dressing.nvim", opts = {} },
 	},
 	-- automatically check for plugin updates
 	checker = { enabled = true },
