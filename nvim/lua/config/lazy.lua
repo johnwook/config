3be8be6c47acc8c9e6f79a2e8f8ebac72d253236
최종욱 <johnwook@choejong-ug-ui-Macmini.local>
2025-01-10 18:20:11 +0900

    follow recommendation of
    https://github.com/zbirenbaum/copilot-cmp?tab=readme-ov-file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -103,6 +103,15 @@ require("lazy").setup({
 				local cmp = require("cmp")
 				local luasnip = require("luasnip")
 
+				local has_words_before = function()
+					if vim.bo.buftype == "prompt" then
+						return false
+					end
+					local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+					return col ~= 0
+						and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+				end
+
 				cmp.setup({
 					snippet = {
 						expand = function(args)
@@ -128,8 +137,8 @@ require("lazy").setup({
 							end
 						end),
 						["<Tab>"] = cmp.mapping(function(fallback)
-							if cmp.visible() then
-								cmp.select_next_item()
+							if cmp.visible() and has_words_before() then
+								cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
 							elseif luasnip.locally_jumpable(1) then
 								luasnip.jump(1)
 							else
@@ -309,12 +318,8 @@ require("lazy").setup({
 			event = "InsertEnter",
 			config = function()
 				require("copilot").setup({
-					panel = {
-						enabled = false,
-					},
-					suggestion = {
-						enabled = false,
-					},
+					panel = { enabled = false },
+					suggestion = { enabled = false },
 				})
 			end,
 		},
