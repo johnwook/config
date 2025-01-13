fffb67a6ad8a0a18a9d11bb42dc8e2bd0dcbbe0d
Johnwook Choi <johnwook.choi@gmail.com>
2025-01-13 15:40:26 +0900

    lsp keymap

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -338,13 +338,13 @@ require("lazy").setup({
 	checker = { enabled = true },
 })
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig_defaults = require("lspconfig").util.default_config
+lspconfig_defaults.capabilities =
+	vim.tbl_deep_extend("force", lspconfig_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 require("mason-lspconfig").setup_handlers({
 	function(server_name)
-		require("lspconfig")[server_name].setup({
-			capabilities = capabilities,
-		})
+		require("lspconfig")[server_name].setup({})
 	end,
 })
 
@@ -360,6 +360,23 @@ require("nvim-treesitter.configs").setup({
 		end,
 		additional_vim_regex_highlighting = false,
 	},
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	end,
 })
 
 -- Fzflua
