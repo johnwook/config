e0f9f20ddd88400ab220411358e0f7878033d695
Johnwook Choi <johnwook.choi@gmail.com>
2025-05-20 14:23:44 +0900

    updated

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -276,11 +276,11 @@ require("lazy").setup({
 			opts = {},
       -- stylua: ignore
       keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-        { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-        { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        { "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+        { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+        { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+        { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
       },
 		},
 		{
@@ -422,7 +422,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local opts = { buffer = event.buf }
 		for _, client in ipairs(vim.lsp.get_clients({ buffer = event.buf })) do -- event.buf 추가
 			if client.name == "eslint" then
-				vim.keymap.set("n", "<leader>ef", "<cmd>EslintFixAll<cr>", opts)
+				vim.keymap.set("n", "<leader>ef", "<cmd>LspEslintFixAll<cr>", opts)
 				break
 			end
 		end
